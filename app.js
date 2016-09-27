602d029ab0584e92d7d217efabf8054cdc97dca3
Wendell Beckwith <wendell.beckwith@ca.com>
2016-09-27 10:57:45 -0500

    Completed chapter 1

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var myModule = angular.module('Angello', []);
 
 myModule.factory('AngelloHelper', function() {
-    var buildIndex = function (source, property) {
+    var buildIndex = function(source, property) {
         var tempArray = [];
 
         for (var i = 0, len = source.length; i < len; ++i) {
-            tempArray[source[i][property]] = source[i];
+            var index = source[i][property];
+            tempArray[index] = source[i];
         }
-
         return tempArray;
     };
 
@@ -18,84 +18,49 @@ myModule.factory('AngelloHelper', function() {
 
 myModule.service('AngelloModel', function() {
     var service = this,
-        statuses = [
-            {name: 'Back Log'},
-            {name: 'To Do'},
-            {name: 'In Progress'},
-            {name: 'Code Review'},
-            {name: 'QA Review'},
-            {name: 'Verified'},
-            {name: 'Done'}
-        ],
-        types = [
-            {name: 'Feature'},
-            {name: 'Enhancement'},
-            {name: 'Bug'},
-            {name: 'Spike'}
-        ],
-        stories = [
-            {
-                title: 'First story',
-                description: 'Our first story.',
-                criteria: 'Criteria pending.',
-                status: 'To Do',
-                type: 'Feature',
-                reporter: 'Lukas Ruebbelke',
-                assignee: 'Brian Ford'
-            },
-            {
-                title: 'Second story',
-                description: 'Do something.',
-                criteria: 'Criteria pending.',
-                status: 'Back Log',
-                type: 'Feature',
-                reporter: 'Lukas Ruebbelke',
-                assignee: 'Brian Ford'
-            },
-            {
-                title: 'Another story',
-                description: 'Just one more.',
-                criteria: 'Criteria pending.',
-                status: 'Code Review',
-                type: 'Enhancement',
-                reporter: 'Lukas Ruebbelke',
-                assignee: 'Brian Ford'
-            }
-        ];
-
-    service.getStatuses = function () {
-        return statuses;
-    };
-
-    service.getTypes = function () {
-        return types;
-    };
+    stories = [
+        {
+            title: 'First story',
+            description: 'Our first story',
+            citeria: 'Criteria pending.',
+            status: 'TO DO',
+            type: 'Feature',
+            reporter: 'Lukas Ruebbelke',
+            assignee: 'Brian Ford'
+        },
+        {
+            title: 'Send story',
+            description: 'Do Something',
+            citeria: 'Criteria pending.',
+            status: 'Back Log',
+            type: 'Feature',
+            reporter: 'Lukas Ruebbelke',
+            assignee: 'Brian Ford'
+        },
+        {
+            title: 'Another story',
+            description: 'Just one more',
+            citeria: 'Criteria pending.',
+            status: 'Code Review',
+            type: 'Enhancement',
+            reporter: 'Lukas Ruebbelke',
+            assignee: 'Brian Ford'
+        }
+    ];
 
     service.getStories = function () {
         return stories;
-    };
+    }
 });
 
-myModule.controller('MainCtrl', function(AngelloModel, AngelloHelper) {
+myModule.controller('MainCtrl', function(AngelloModel) {
     var main = this;
-
-    main.types = AngelloModel.getTypes();
-    main.statuses = AngelloModel.getStatuses();
-    main.stories = AngelloModel.getStories();
-    main.typesIndex = AngelloHelper.buildIndex(main.types, 'name');
-    main.statusesIndex = AngelloHelper.buildIndex(main.statuses, 'name');
-
-    main.setCurrentStory = function (story) {
-        main.currentStory = story;
-        main.currentStatus = main.statusesIndex[story.status];
-        main.currentType = main.typesIndex[story.type];
-    };
 
     main.createStory = function() {
         main.stories.push({
             title: 'New Story',
-            description: 'Description pending.',
-            criteria: 'Criteria pending.',
+            description: 'Description pending',
+            citeria: 'Criteria pending.',
             status: 'Back Log',
             type: 'Feature',
             reporter: 'Pending',
@@ -103,23 +68,14 @@ myModule.controller('MainCtrl', function(AngelloModel, AngelloHelper) {
         });
     };
 
-    main.setCurrentStatus = function (status) {
-        if (typeof main.currentStory !== 'undefined') {
-            main.currentStory.status = status.name;
-        }
-    };
-
-    main.setCurrentType = function (type) {
-        if (typeof main.currentStory !== 'undefined') {
-            main.currentStory.type = type.name;
-        }
-    };
+    main.stories = AngelloModel.getStories()
 });
 
 myModule.directive('story', function() {
     return {
         scope: true,
         replace: true,
-        template: '<div><h4>{{story.title}}</h4><p>{{story.description}}</p></div>'
-    }
+        template: '<div><h4>{{story.title}}</h4>' +
+        '<p>{{story.description}}</p></div>'
+    };
 });
